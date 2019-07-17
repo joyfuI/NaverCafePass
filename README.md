6f13f8ddec198c40014a740d3079f6281e325300
joyfuI <jong970105@gmail.com>
2019-07-17 14:46:36 +0900

    ArticleRead.nhn 페이지에서도 작동하도록 리다이렉션 추가
    대신 동기식 XMLHttpRequest가 사용되면서 경고가 뜸.
    잘하면 프레임 이동한 것도 작동되게 할 수 있을 것 같은데 잘 안되네...

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * [Chrome](https://chrome.google.com/webstore/detail/%EB%84%A4%EC%9D%B4%EB%B2%84-%EC%B9%B4%ED%8E%98-%ED%8C%A8%EC%8A%A4/gipgjcnhbklggnannochejcaieghkmcn)
 * [Whale](https://store.whale.naver.com/detail/jbhpjfbiohcbmahilfnkaencepaaohmn)
 
-아니 이놈의 엣지랑 오페라는 대체 언제 승인해주는거야? 몇달째 반응이 없네ㅡㅡ
+엣지랑 오페라에도 등록 신청을 해두었으나 몇 달째 반응이 없음...
 ## 지원 브라우저
 * [Firefox](https://www.mozilla.org/)
 * [Chrome](https://www.google.com/chrome/)<!-- * [Edge](https://www.microsoft.com/ko-kr/windows/microsoft-edge) -->
@@ -14,3 +14,6 @@
 
 이외에도 지원했으면 좋겠다 싶은 브라우저가 있으면 알려주세요.  
 WebExtensions API를 지원하는 브라우저면 아마도 지원이 가능할 겁니다.
+## 사용 팁
+카페 게시글 목록에서도 작동이 되긴 합니다만 새 탭으로 열어야 작동됩니다.  
+그냥 클릭(즉, 프레임 이동) 시 미작동. 모바일 페이지에선 상관 없습니다.
--- src/background.js
+++ src/background.js
@@ -1,9 +1,9 @@
 "use strict";
-function referer(details) {	//cafe-intro
-	const refererUrl = 'https://search.naver.com/?&query=a';
-	let toggle = true;
+function referer(details) {
+	const refererUrl = 'https://search.naver.com/?&query=.';	// 모바일 페이지에선 query 매개변수 필요
 
-	if ((/\/.+\/\d+/).test(details.url)) {	// 게시글 주소에서만 작동
+	if ((/(cafe.naver.com\/.+\/\d+)|(m.cafe.naver.com\/ArticleRead.nhn.+articleid.+)/).test(details.url)) {	// 게시글 주소에서만 작동
+		let toggle = true;
 		for (let header of details.requestHeaders) {
 			if (header.name.toLowerCase() === 'referer') {	// 리퍼러가 있으면
 				header.value = refererUrl;	// 네이버 검색 페이지로 조작
@@ -15,19 +15,57 @@ function referer(details) {	//cafe-intro
 			details.requestHeaders.push({ name: 'Referer', value: refererUrl });	// 리퍼러 추가
 		}
 	}
-	return {requestHeaders: details.requestHeaders};
+	return { requestHeaders: details.requestHeaders };
 }
 
-let filter = { urls: ['*://cafe.naver.com/*', '*://m.cafe.naver.com/*'] };
-let extraInfoSpec = ['blocking', 'requestHeaders'];
+function replace(details) {
+	if (details.type != 'main_frame') {	// 프레임 내부에선 작동 안하도록
+		return;
+	}
 
+	let clubid = details.url.match(/clubid=(\d+)/)[1];
+	let articleid = details.url.match(/articleid=(\d+)/)[1];
+	let cafeName;
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://cafe.naver.com/MyCafeMain.nhn?clubid=' + clubid, false);
+	xhr.send(null);
+	if (xhr.status === 200) {
+		cafeName = xhr.responseText.match(/var g_sCafeHome = \"https:\/\/cafe.naver.com\/\" \+ \"(.+)\"/)[1];	// 카페 이름 추출
+	} else {
+		return;
+	}
+/*
+	if (details.type == 'sub_frame') {
+		if (details.originUrl.indexOf('ArticleList.nhn') != -1) {
+			let xhr = new XMLHttpRequest();
+			xhr.open('GET', 'https://cafe.naver.com/' + cafeName + '/' + articleid, false);
+			xhr.send(null);
+			if (xhr.status === 200) {
+				let sc = xhr.responseText.match(/\$\(\"cafe_main\"\)\.src = \"(.+)\";/)[1];
+				console.log(sc)
+				return { redirectUrl: 'https:' + sc };
+			}
+		}
+		return;
+	}
+*/
+	return { redirectUrl: 'https://cafe.naver.com/' + cafeName + '/' + articleid };
+}
+
+let extraInfoSpec = ['blocking', 'requestHeaders'];
 if (typeof(browser) === 'undefined') {	// 파이어폭스는 browser, chrome 네임스페이스 모두 지원하는데 크롬은 chrome만 지원. 또, 엣지는 browser만 지원
 	var browser = chrome;
 	extraInfoSpec.push('extraHeaders');	// 크롬용 옵션
 }
+
+let filter = { urls: ['*://cafe.naver.com/*', '*://m.cafe.naver.com/*'] };
 try {
 	browser.webRequest.onBeforeSendHeaders.addListener(referer, filter, extraInfoSpec);
 } catch (e) {	// extraHeaders가 크롬 72 버전부터 추가된거라 구 크로뮴 엔진을 쓰면 에러 발생ㅡㅡ
 	extraInfoSpec.pop();
 	browser.webRequest.onBeforeSendHeaders.addListener(referer, filter, extraInfoSpec);
 }
+
+browser.webRequest.onBeforeRequest.addListener(replace, {
+	urls: ["*://cafe.naver.com/ArticleRead.nhn*articleid*"]
+}, ['blocking']);
